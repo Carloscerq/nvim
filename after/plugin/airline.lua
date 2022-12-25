80b3ac70ab43d45f7482a86e338dfaaf3d0d1ba4
Carlos Cerqueira <ce.cesc01@gmail.com>
2022-12-24 22:58:45 -0300

    Initial commit

---
+++ after/plugin/airline.lua
@@ -0,0 +1,2 @@
+vim.cmd("let g:airline#extensions#tabline#enabled = 1")
+vim.cmd([[let g:airline_theme = 'nord_minimal']])
---
+++ lua/config/set.lua
@@ -0,0 +1,15 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.scrolloff = 3
+vim.opt.showmode = true
+vim.opt.wrap = true
+vim.opt.swapfile = false
+vim.opt.incsearch = true
+vim.opt.smartindent = true
+vim.opt.updatetime = 50
+vim.opt.termguicolors = true
