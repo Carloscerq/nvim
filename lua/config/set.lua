fefd6e2419fc61601024f73e925e785adf75d4d5
Carlos Cerqueira <ce.cesc01@gmail.com>
2023-03-29 12:12:33 -0300

    feat: add clipboard

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,6 +1,5 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -13,3 +12,4 @@ vim.opt.incsearch = true
 vim.opt.smartindent = true
 vim.opt.updatetime = 50
 vim.opt.termguicolors = true
+vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
